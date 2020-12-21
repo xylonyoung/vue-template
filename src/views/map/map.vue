250cbb544cf01a2e494e8ec8ba341118f15cc73f
xylon <xylonyoung@live.com>
2020-12-21 10:26:28 +0800

    update

--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -9,12 +9,11 @@ export default {
     return {
       width: 1280,
       height: 720,
-      scale: 600,
+      scale: 500,
       url: 'https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json',
     }
   },
   mounted() {
-    this.$nextTick(() => {})
     const svg = d3
       .select('#d3Map')
       .append('svg')
@@ -22,35 +21,51 @@ export default {
       .attr('height', this.height)
     const projection = d3
       .geoMercator()
+      .center([105, 31])
       .scale(this.scale)
-      .center([105, 38])
       .translate([this.width / 2, this.height / 2])
     const path = d3.geoPath().projection(projection)
 
     const color = d3.scaleOrdinal(d3.schemeCategory10)
-    // const color = d3.schemeCategory10
 
-    d3.json(this.url).then(res => {
-      svg
-        .selectAll('g')
-        .data(res.features)
-        .enter()
-        .append('g')
-        .append('path')
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1)
-        .attr('opacity', 0.6)
-        .attr('fill', function (d, i) {
-          return color(i)
-        })
-        .attr('d', path)
-        .on('mouseover', function () {
-          d3.select(this).attr('opacity', 1)
-        })
-        .on('mouseout', function () {
-          d3.select(this).attr('opacity', 0.6)
-        })
-    })
+    const chinaJson = require('@/assets/china.json')
+
+    // let mapData = chinaJson.features.filter(m => m.properties.name)
+
+    // mapData = mapData.map(item => {
+    //   if (item.properties.name === '海南省') {
+    //     return {
+    //       ...item,
+    //       geometry: {
+    //         ...item.geometry,
+    //         coordinates: [item.geometry.coordinates[0]],
+    //       },
+    //     }
+    //   }
+    //   return {
+    //     ...item,
+    //   }
+    // })
+
+    svg
+      .append('g')
+      .selectAll('path')
+      .data(chinaJson.features)
+      .enter()
+      .append('path')
+      .attr('stroke', '#000')
+      .attr('stroke-width', 1)
+      .attr('fill', function (d, i) {
+        return color(i)
+      })
+      .attr('d', path)
+    // .attr('opacity', 0.6)
+    // .on('mouseover', function () {
+    //   d3.select(this).attr('opacity', 1)
+    // })
+    // .on('mouseout', function () {
+    //   d3.select(this).attr('opacity', 0.6)
+    // })
   },
 }
 </script>
