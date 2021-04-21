6a576f3cc2ab5165d2275f2b5130c73a5e647bdd
xylon <xylonyoung@live.com>
2021-04-21 17:41:12 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,8 +31,6 @@ module.exports = {
       {
         semi: false,
         singleQuote: true,
-        trailingComma: 'none',
-        arrowParens: 'avoid',
         htmlWhitespaceSensitivity:"ignore"
       }
     ]
